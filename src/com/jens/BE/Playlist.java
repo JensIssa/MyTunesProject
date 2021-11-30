50680bbe2631bf7aea0d5c1016cfc3cf41f9ece0
Zeroject <cypeyt@gmail.com>
2021-11-30 09:45:12 +0100

    BE layer Complete
    Done with The be layer playlist and song objects soo the project is online

--- src/com/jens/BE/Playlist.java
+++ src/com/jens/BE/Playlist.java
@@ -1,4 +1,36 @@
 package com.jens.BE;
 
+import javafx.collections.ObservableList;
+
 public class Playlist {
+
+    private String playlistName;
+    private float totalTime;
+    private ObservableList<Song> songsInPlaylist;
+
+    public Playlist(String playlistName, float totalTime, ObservableList<Song> songsInPlaylist){
+        this.playlistName = playlistName;
+        this.totalTime = totalTime;
+        this.songsInPlaylist = songsInPlaylist;
+    }
+
+    public float getTotalTime()
+    {
+        return totalTime;
+    }
+
+    public void setPlaylistName(String playlistName)
+    {
+        this.playlistName = playlistName;
+    }
+
+    public String getPlaylistName()
+    {
+        return playlistName;
+    }
+
+    public ObservableList<Song> getSongsInPlaylist()
+    {
+        return songsInPlaylist;
+    }
 }
--- src/com/jens/BE/Song.java
+++ src/com/jens/BE/Song.java
@@ -1,4 +1,66 @@
 package com.jens.BE;
 
 public class Song {
+
+    private String title;
+    private String artistName;
+    private float lentgh;
+    private String catergory;
+
+    public Song(String title, String artistName, float lentgh, String catergory){
+        this.title = title;
+        this.artistName = artistName;
+        this.lentgh = lentgh;
+        this.catergory = catergory;
+    }
+
+    public String getTitle()
+    {
+        return title;
+    }
+
+    public void setTitle(String title)
+    {
+        this.title = title;
+    }
+
+    public String getArtistName()
+    {
+        return artistName;
+    }
+
+    public void setArtistName(String artistName)
+    {
+        this.artistName = artistName;
+    }
+
+    public float getLentgh()
+    {
+        return lentgh;
+    }
+
+    public void setLentgh(float lentgh)
+    {
+        this.lentgh = lentgh;
+    }
+
+    public String getCatergory()
+    {
+        return catergory;
+    }
+
+    public void setCatergory(String catergory)
+    {
+        this.catergory = catergory;
+    }
+
+    @Override public String toString()
+    {
+        return "Song{" +
+                "title='" + title + '\'' +
+                ", artistName='" + artistName + '\'' +
+                ", lentgh=" + lentgh +
+                ", catergory='" + catergory + '\'' +
+                '}';
+    }
 }
