3ec89a22b39048839093e1be0545f53119c22d1f
Ominousity <abs@hotmail.dk>
2021-11-30 13:07:55 +0100

    added icons

--- src/com/jens/GUI/Controller/MainWindowController.java
+++ src/com/jens/GUI/Controller/MainWindowController.java
@@ -1,18 +1,45 @@
 package com.jens.GUI.Controller;
 
-import javafx.scene.control.Button;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
 
-import java.util.Objects;
+import com.jens.BE.Song;
+import javafx.event.ActionEvent;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 
-public class MainWindowController {
+import java.net.URL;
+import java.util.ResourceBundle;
 
-    public Button moveUp;
-    public Button moveDown;
-    public Button directAddSongToPlaylist;
+public class MainWindowController implements Initializable {
+
+    public TableColumn songTitleColumn;
+    public TableColumn songArtistColumn;
+    public TableColumn songCategoryColumn;
+    public TableColumn songTimeColumn;
+    public TableView songTable;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        songTitleColumn.setCellValueFactory(new PropertyValueFactory<Song, String>("Title"));
+        songArtistColumn.setCellValueFactory(new PropertyValueFactory<Song, String>("Title"));
+        songCategoryColumn.setCellValueFactory(new PropertyValueFactory<Song, String>("Title"));
+        songTimeColumn.setCellValueFactory(new PropertyValueFactory<Song, String>("Title"));
+        songTable.setItems(getSongs());
+    }
 
     public MainWindowController() {
 
     }
+
+    public void newSong(ActionEvent actionEvent) {
+
+    }
+
+    public void editSong(ActionEvent actionEvent) {
+    }
+
+    public void deleteSong(ActionEvent actionEvent) {
+    }
+
 }
