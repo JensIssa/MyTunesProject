8ca4658172466efeb468035ea030b1038c052088
Jens Bassem Hanna Issa <jens7391@easv365.dk>
2021-11-30 12:44:55 +0100

    made getAllSongs and createSong

--- src/com/jens/DAL/SongDAO.java
+++ src/com/jens/DAL/SongDAO.java
@@ -29,7 +29,7 @@ public class SongDAO {
                 while (rs.next()){
                     int id = rs.getInt("Id");
                     String title = rs.getString("Title");
-                    String artistName = rs.getString("AristName");
+                    String artistName = rs.getString("ArtistName");
                     float songLength = rs.getFloat("SongLength");
                     String category = rs.getString("Category");
                     String url = rs.getString("Url");
@@ -42,7 +42,29 @@ public class SongDAO {
         }
         return songs;
     }
-
+    public Song createSong(String title, String artistName, float songLength, String category, String url ) throws SQLException {
+        String sql = "INSERT INTO SONG(Title, ArtistName, SongLength, Category, Url) values (?,?,?,?,?);";
+        Connection connection = connectionPool.checkOut();
+        try(PreparedStatement preparedStatement = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)){
+            preparedStatement.setString(1, title);
+            preparedStatement.setString(2, artistName);
+            preparedStatement.setFloat(3, songLength);
+            preparedStatement.setString(4, category);
+            preparedStatement.setString(5, url);
+            preparedStatement.executeUpdate();
+            ResultSet resultSet = preparedStatement.getGeneratedKeys();
+            int id = 0;
+            if(resultSet.next()){
+                id = resultSet.getInt(1);
+            }
+            Song song = new Song(id, title, artistName, songLength, category, url);
+            return song;
+        }
+        catch (SQLException throwables){
+            throwables.printStackTrace();
+        }
+        return null;
+    }
 }
 
 
