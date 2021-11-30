6e6fedc1dec686010375f95666df009fccd3d4bf
Zeroject <cypeyt@gmail.com>
2021-11-30 10:59:10 +0100

    Update SongSearcher.java

--- src/com/jens/BLL/util/SongSearcher.java
+++ src/com/jens/BLL/util/SongSearcher.java
@@ -20,7 +20,7 @@ public class SongSearcher {
     }
 
     private boolean compareToMovieYear(String query, Song song) {
-        return Float.toString(song.getLentgh()).contains(query);
+        return Float.toString(song.getsongLength()).contains(query);
     }
 
     private boolean compareToMovieTitle(String query, Song song) {
