510335834becf92fc2957fc7d3c3c7ef10990bcd
Ominousity <abs@hotmail.dk>
2021-11-30 10:57:17 +0100

    added stuff
    added MainWindow and controller

--- src/com/jens/GUI/Controller/MainWindowController.java
+++ src/com/jens/GUI/Controller/MainWindowController.java
@@ -1,4 +1,19 @@
 package com.jens.GUI.Controller;
 
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
 public class MainWindowController {
+
+    public Button moveUp;
+    public Button moveDown;
+    public Button directAddSongToPlaylist;
+
+    public MainWindowController() {
+        Image img = new Image("");
+        ImageView view = new ImageView(img);
+
+        moveUp.setGraphic(view);
+    }
 }
