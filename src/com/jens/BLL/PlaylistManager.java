98ae866be21f73d17116b2e7677a0430e6685b14
Zeroject <cypeyt@gmail.com>
2021-11-30 10:30:53 +0100

    Fixed Song/Playlist and added function to Managers in be layer and BLL
    playlist and song now has an id and playlist no longer have an list in it.
    
    Managers now have the basic structures from the manager interface

--- src/com/jens/BLL/PlaylistManager.java
+++ src/com/jens/BLL/PlaylistManager.java
@@ -1,4 +1,32 @@
 package com.jens.BLL;
 
+import com.jens.BE.Playlist;
+import com.jens.BE.Song;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class PlaylistManager implements Manager{
+
+    List<Song> songList = new ArrayList<>();
+
+    @Override public String setName(String name)
+    {
+        return name;
+    }
+
+    @Override public String getName(String name)
+    {
+        return name;
+    }
+
+    @Override public float time(float time)
+    {
+        float totaltime = 0;
+        //for loop for songs in database JENS PLEASE FIX
+        for(Song song : songList){
+            totaltime = totaltime + song.getLentgh();
+        }
+        return totaltime;
+    }
 }
--- src/com/jens/BLL/SongManager.java
+++ src/com/jens/BLL/SongManager.java
@@ -1,4 +1,18 @@
 package com.jens.BLL;
 
 public class SongManager implements Manager{
+    @Override public String setName(String name)
+    {
+        return name;
+    }
+
+    @Override public String getName(String name)
+    {
+        return name;
+    }
+
+    @Override public float time(float time)
+    {
+        return time;
+    }
 }
