c509d56b3db2e76cf8a1b5bec1dedca8e1afc911
Ominousity <abs@hotmail.dk>
2021-11-30 15:18:51 +0100

    Added the last GUI

--- src/com/jens/GUI/Controller/Add_EditSongController.java
+++ src/com/jens/GUI/Controller/Add_EditSongController.java
@@ -1,4 +1,16 @@
 package com.jens.GUI.Controller;
 
+import javafx.event.ActionEvent;
+
 public class Add_EditSongController {
+    
+    
+    public void CancelNew_Edit(ActionEvent actionEvent) {
+    }
+
+    public void addSong(ActionEvent actionEvent) {
+    }
+
+    public void chooseSongFile(ActionEvent actionEvent) {
+    }
 }
