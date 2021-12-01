351a627c1a1ade4b9e07830bc9a02b590c32288d
Zeroject <cypeyt@gmail.com>
2021-12-01 10:57:30 +0100

    Update SongManager.java
    CREATE, UPDATE, DELETE AND ALL SONGS ARE NOW READY TO SONG

--- src/com/jens/BLL/SongManager.java
+++ src/com/jens/BLL/SongManager.java
@@ -1,6 +1,19 @@
 package com.jens.BLL;
 
+import com.jens.BE.Song;
+import com.jens.DAL.SongDAO;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
 public class SongManager implements Manager{
+
+    SongDAO songDAO = new SongDAO();
+
+    public SongManager() throws IOException
+    {
+    }
+
     @Override public String setName(String name)
     {
         return name;
@@ -14,5 +27,24 @@ public class SongManager implements Manager{
     @Override public float time(float time)
     {
         return time;
+    }
+
+    public void createSong(String title, String artistName, float songLength, String category, String url) throws SQLException
+    {
+        songDAO.createSong(title, artistName, songLength, category, url);
+    }
+
+    public void updateSong(Song song) throws SQLException
+    {
+        songDAO.updateSong(song);
+    }
+
+    public void deleteSong(Song song){
+        songDAO.deleteSong(song);
+    }
+
+    public void getAllSongs() throws SQLException, IOException
+    {
+        songDAO.getAllSongs();
     }
 }
