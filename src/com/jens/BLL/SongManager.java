be2b1220f516ec2c59da346378d2bfbd851ac4d5
Jens Bassem Hanna Issa <jens7391@easv365.dk>
2021-12-06 14:13:35 +0100

    added songSearcher method to SongManager

--- src/com/jens/BLL/SongManager.java
+++ src/com/jens/BLL/SongManager.java
@@ -1,6 +1,7 @@
 package com.jens.BLL;
 
 import com.jens.BE.Song;
+import com.jens.BLL.util.SongSearcher;
 import com.jens.DAL.SongDAO;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -12,10 +13,13 @@ import java.util.List;
 
 public class SongManager{
 
-    SongDAO songDAO = new SongDAO();
+    SongDAO songDAO;
+    SongSearcher songSearcher;
 
     public SongManager() throws IOException
     {
+        songSearcher = new SongSearcher();
+        songDAO = new SongDAO();
     }
 
     public void createSong(String title, String artistName, int songLength, String category, String url) throws SQLException
@@ -35,5 +39,12 @@ public class SongManager{
     public List<Song> getAllSongs() throws SQLException, IOException
     {
         return songDAO.getAllSongs();
+    }
+
+    public ObservableList<Song> search(ObservableList<Song> searchBase, String query )
+    {
+        ObservableList<Song> songsFound = FXCollections.observableArrayList();
+        songsFound.addAll(songSearcher.search(searchBase, query));
+        return songsFound;
     }
 }
