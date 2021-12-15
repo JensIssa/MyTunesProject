ffdfc25df5f734011c944dd2066fe0a5aa16d613
Ominousity <abs@hotmail.dk>
2021-12-15 13:19:16 +0100

    more trash

--- src/com/jens/GUI/Model/SongModel.java
+++ src/com/jens/GUI/Model/SongModel.java
@@ -2,31 +2,38 @@ package com.jens.GUI.Model;
 
 import com.jens.BE.Song;
 import com.jens.BLL.SongManager;
-import com.jens.BLL.util.MusicPlayer;
-import com.jens.GUI.MainWindowController;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import javafx.scene.control.TableView;
+
 
 import java.io.IOException;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Timer;
+
 
 public class SongModel {
 
     private SongManager songManager = new SongManager();
-    private MusicPlayer musicPlayer;
     private ObservableList<Song> songList;
 
+    /**
+     * Instantiates the songlist
+     * @throws IOException
+     */
     public SongModel() throws IOException {
         songList = FXCollections.observableArrayList();
     }
 
+    /**
+     * Converts the list of songs into an observable list
+     * @return returns an observable list
+     * @throws SQLException
+     * @throws IOException
+     */
     public ObservableList<Song> listToObservablelist() throws SQLException, IOException {
 
-        List<Song> tempSong = new ArrayList<>();
+        List<Song> tempSong;
         tempSong = this.songManager.getAllSongs();
         for (Song song : tempSong)
         {
@@ -35,23 +42,55 @@ public class SongModel {
         return songList;
     }
 
+    /**
+     * Creates a song
+     * @param title Name of the song
+     * @param artistName Name of the artist
+     * @param songLength Length of the song
+     * @param category The song genre
+     * @param url The songs local filePath
+     * @param urlImg The songs image filePath
+     * @throws SQLException
+     */
     public void createSong(String title, String artistName, int songLength, String category, String url, String urlImg) throws SQLException {
         songManager.createSong(title, artistName, songLength, category, url, urlImg);
     }
 
+    /**
+     * Deletes the selected song
+     * @param song The selected song
+     */
     public void deleteSong(Song song){
         songManager.deleteSong(song);
     }
 
+    /**
+     * 
+     * @param query
+     * @throws SQLException
+     * @throws IOException
+     */
     public void searchSongs(String query) throws SQLException, IOException
     {
         List<Song> searchResults = songManager.searchSongs(query);
         songList.clear();
         songList.addAll(searchResults);
     }
+
+    /**
+     *
+     * @param song
+     * @return
+     */
     public String songImageUpdate(Song song){
         return songManager.updateSongImage(song);
     }
+
+    /**
+     * Updates the songs properties
+     * @param song The selected song
+     * @throws SQLException
+     */
     public void updateSong (Song song) throws SQLException {
         songManager.updateSong(song);
     }
